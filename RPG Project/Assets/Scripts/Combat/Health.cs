d5d645f46bbc49d43a98e1fc427666dd9c4641d3
jpeoples <jpeoples@expediagroup.com>
2020-02-24 13:09:05 -0800

    add Death animation

--- RPG Project/Assets/Scripts/Combat/Health.cs
+++ RPG Project/Assets/Scripts/Combat/Health.cs
@@ -4,12 +4,26 @@ namespace RPG.Combat {
 
     public class Health : MonoBehaviour {
 
-       [SerializeField] float health = 100f;
+       [SerializeField] float healthPoints = 100f;
+
+       bool isDead = false;
 
        public void TakeDamage(float damage){
 
-           health = Mathf.Max(health - damage, 0);
-           print(health);
-       }
+           healthPoints = Mathf.Max(healthPoints - damage, 0);
+           if(healthPoints == 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            // Don't do anythig following if isDead is true; 
+            if(isDead) return;
+
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("die");
+        }
     }
 }
